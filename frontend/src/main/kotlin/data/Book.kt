bd10fec6a0ca3f3f3da32d2265ef8e1486657b1c
ZYH <1427273594@qq.com>
2024-09-04 00:53:47 +0800

    feat:新增查看借阅信息功能基础部分

--- frontend/src/main/kotlin/data/Book.kt
+++ frontend/src/main/kotlin/data/Book.kt
@@ -2,22 +2,27 @@
 package data
 
 data class Book(
-    val coverImage: String,
+    val coverImage: String = "",
 
-    val bookname: String,
-    val author: String,
+    val bookname: String = "",
+    val author: String = "",
 
-    val publisher: String,
-    val publishDate: String,
+    val publisher: String = "",
+    val publishDate: String = "",
 
-    val language: String,
+    val language: String = "",
 
-    val isbn: String,
-    val description: String,
+    val isbn: String = "",
+    val description: String = "",
 
-    val kind: String,
-    val quantity: Int,
-    val valid: Int
+    val kind: String = "",
+    val quantity: Int = 0,
+    val valid: Int = 0,
+
+    val borrow_date: String = "",
+    val return_date: String = "",
+
+    val condition: String = ""
 ) {
 
 }
