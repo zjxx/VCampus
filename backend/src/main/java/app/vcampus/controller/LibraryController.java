b8f7ad359b0f8a44375faf0e4f057cda1a825a1a
yan_tai <213221537@seu.edu.cn>
2024-09-06 17:02:11 +0800

    fix:addbook

--- backend/src/main/java/app/vcampus/controller/LibraryController.java
+++ backend/src/main/java/app/vcampus/controller/LibraryController.java
@@ -418,7 +418,7 @@ public class LibraryController {
                 data.addProperty("reason", "The book already exists.");
                 return gson.toJson(data);
             }
-            String filepath= request.get("ISBN").getAsString() + ".jpg";
+            String filepath="C:\\Users\\Administrator\\Desktop\\server\\img\\"+ request.get("ISBN").getAsString() + ".jpg";
             fileOutputStream = new FileOutputStream(filepath);//指定保持路径
             byte[] bytes = java.util.Base64.getDecoder().decode(additionalParam);
             fileOutputStream.write(bytes);
