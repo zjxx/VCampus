3cd5de8a33f1dd1b53719bd80ee0563448ee5666
chen-symx <1574810719@qq.com>
2024-09-11 11:25:23 +0800

    feat: 修改选课信息

--- frontend/src/main/kotlin/view/component/DetailCard.kt
+++ frontend/src/main/kotlin/view/component/DetailCard.kt
@@ -25,10 +25,9 @@ fun DetailCard(course: Course, onSelectCourse: (Course, onSuccess: (Boolean) -> 
     ) {
         Column(modifier = Modifier.padding(8.dp)) {
             Text("教师: ${course.teacher}", fontWeight = FontWeight.Bold)
-            course.timeSlots.forEach { timeSlot ->
-                Text("周次: ${timeSlot.week} 第${timeSlot.begin}-${timeSlot.end}节", fontWeight = FontWeight.Bold)
+            course.timeAndLocationCards.forEach { timeAndLocationCard ->
+                Text(" ${timeAndLocationCard.dayOfWeek} 第${timeAndLocationCard.startPeriod}-${timeAndLocationCard.endPeriod}节  教室: ${timeAndLocationCard.location}", fontWeight = FontWeight.Bold)
             }
-            Text("教室: ${course.location}", fontWeight = FontWeight.Bold)
             Text("课容量: ${course.capacity}", fontWeight = FontWeight.Bold)
             Text("剩余课容量: $validCapacity", fontWeight = FontWeight.Bold) // Display updated validCapacity
             Spacer(modifier = Modifier.height(8.dp))
