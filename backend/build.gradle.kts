22da45b9b53ed0da666033a36d14b3b3f4aead2f
yan_tai <213221537@seu.edu.cn>
2024-09-13 11:45:08 +0800

    fix:马老师为什么是神

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,14 +1,13 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
-
 group = "app.vcampus"
-version = "1.0-SNAPSHOT"
+version = "1.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
-
 dependencies {
     implementation("mysql:mysql-connector-java:8.0.33")
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
@@ -19,21 +18,16 @@ dependencies {
     implementation("org.bouncycastle:bcpkix-jdk15on:1.65")
 
 }
-
-
-
-
 tasks.test {
     useJUnitPlatform()
 
 }
-
-
 tasks.withType<Jar> {
     manifest {
         attributes["Main-Class"] = "app.vcampus.Main"
     }
-
+    isZip64 = true
+    exclude("META-INF/*.RSA", "META-INF/*.SF", "META-INF/*.DSA")
     doFirst {
         from(configurations.runtimeClasspath.get().map {
             if (it.isDirectory) it else zipTree(it)
